a6ad0af25fccb217e0d0f69e7f8a271016087e7e
Stepachka <stepan.talabaev@mail.ru>
2025-09-15 17:15:06 +0300

    feat: test done terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,48 +7,23 @@ terraform {
   required_version = ">= 0.13"
 }
 
-provider "timeweb" {
-  token = var.timeweb_token
+data "twc_configurator" "configurator" {
+  location = "ru-1"
+  disk_type = "nvme"
 }
 
-# Создаем виртуальную машину (конфигурация Cloud-30)
-resource "timeweb_server" "online_shop" {
-  name   = "online-shop-server"
-  image  = "ubuntu-22.04"
-  cpu    = 1
-  ram    = 2048
-  disk   = 30
-  region = "ru-1"
+data "twc_os" "os" {
+  name = "ubuntu"
+  version = "22.04"
 }
+resource "twc_server" "example-server" {
+  name = "Example server"
+  os_id = data.twc_os.os.id
 
-# Открываем необходимые порты
-resource "timeweb_firewall_group" "firewall" {
-  name = "online-shop-firewall"
-
-  rule {
-    direction = "in"
-    protocol  = "tcp"
-    port      = "22"
-    cidr      = "0.0.0.0/0"
+  configuration {
+    configurator_id = data.twc_configurator.configurator.id
+    disk = 1024 * 15
+    cpu = 1
+    ram = 1024
   }
-
-  rule {
-    direction = "in"
-    protocol  = "tcp"
-    port      = "80"
-    cidr      = "0.0.0.0/0"
-  }
-
-  rule {
-    direction = "in"
-    protocol  = "tcp"
-    port      = "3000"
-    cidr      = "0.0.0.0/0"
-  }
-}
-
-resource "timeweb_server_firewall" "server_firewall" {
-  server_id  = timeweb_server.online_shop.id
-  group_id   = timeweb_firewall_group.firewall.id
-  is_enabled = true
 }
